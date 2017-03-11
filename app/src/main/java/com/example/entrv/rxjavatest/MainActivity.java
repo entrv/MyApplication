a17b73f7fae3a6f1a7325d45e3fea6e51a66d2a6
entrvn7 <entrvn7@gmail.com>
2017-03-11 23:57:01 +0900

    first commit

---
+++ app/src/main/java/com/example/entrv/rxjavatest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.entrv.rxjavatest;
+
+import android.database.Observable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    TextView tv = (TextView) findViewById(R.id.hello_text);
+    Observable.just
+  }
+
+  public String ac() {
+    return "aa";
+  }
+}
